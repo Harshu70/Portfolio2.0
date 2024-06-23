04c8a438f566e0b39cfd2ce72191a24a4f391e9b
Harsh Kumar <harshuraj7070@gmail.com>
2024-06-24 01:58:41 +0530

    edit

--- script.js
+++ script.js
@@ -1,16 +1,10 @@
 function loco() {
     gsap.registerPlugin(ScrollTrigger);
-
-    // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
-
     const locoScroll = new LocomotiveScroll({
         el: document.querySelector("#main"),
         smooth: true
     });
-    // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
     locoScroll.on("scroll", ScrollTrigger.update);
-
-    // tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
     ScrollTrigger.scrollerProxy("#main", {
         scrollTop(value) {
             return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
@@ -20,13 +14,8 @@ function loco() {
         },
         pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
     });
-
-    // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
     ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
-
-    // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
     ScrollTrigger.refresh();
-
 }
 loco();
 
@@ -144,7 +133,7 @@ function loader() {
         .to(".bad, #web", {
             opacity: 1,
             duration: 1,
-            delay: -0.5
+            delay: -1
         })
         .to("#strip", {
             opacity: 1,
@@ -155,7 +144,7 @@ function loader() {
         })
         .from("#page3", {
             opacity: 0,
-            duration: 2
+            duration: 1
         })
 }
 loader();
